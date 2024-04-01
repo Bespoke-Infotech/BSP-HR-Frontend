b6387afcd5fe6fdd29822e1a5d7b5a6339e7f5f4
Adedeji Richards <adewumiadedeji27@gmail.com>
2024-04-01 21:37:18 +0100

    ...

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change to the directory where your application code is located
+cd /hr.bespokecreatives.tech
+
+# Install dependencies using npm
+npm install
+
+# Build your Next.js application (if needed)
+# npm run build
+
+# Start your Node.js server
+npm start
